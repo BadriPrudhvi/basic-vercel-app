5a3927c0d3ac73d4e47d18fae649e12b6a2d4636
prudhvi <prudhvi@cloudflare.com>
2024-06-03 11:51:04 -0500

    added h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Hello, Tailwind CSS with Next.js!</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-4xl font-bold text-blue-800">Hello, Prudhvi!</h1>
+      <h3 className="mt-4 text-2xl font-bold text-orange-400">Congrats!!! Deploying your first application</h3>
     </div>
   );
 }
